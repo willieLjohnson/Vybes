b12731c4b83f938fb350cf38c1183392a2f456d7
SlickJohnson <liwa.johnson@gmail.com>
2018-03-19 15:32:41 -0700

    Layout bare minimum UI using storyboard

---
+++ Vybes/ViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+/// Main View of the app
+class ViewController: UIViewController {
+  /// Textfield for entry input.
+  @IBOutlet weak var entryTextField: UITextField!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
--- Vybes/VyberCell.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-class VyberCell: UITableViewCell {
-    
-    @IBOutlet weak var name: UILabel!
-    @IBOutlet weak var vybe: UILabel!
-}
-

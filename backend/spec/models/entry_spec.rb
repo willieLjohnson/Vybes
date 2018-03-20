9344a54f73ae81de7a6f4c0edd104fd4905197e0
SlickJohnson <liwa.johnson@gmail.com>
2018-03-20 10:37:45 -0700

    Update Entry model tests

--- backend/spec/models/entry_spec.rb
+++ backend/spec/models/entry_spec.rb
@@ -1,27 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
+  subject {
+    User.new(name: "Willie", email: "willies@entries.com")
+  }
+
   describe "Validations" do
     it "is valid with valid attributes" do
-      entry = Entry.new(date: DateTime.now.utc, )
-      expect(user).to be_valid
+      entry = Entry.new(date: DateTime.now.utc, body: "I have a date and user!", user: subject)
+      expect(entry).to be_valid
     end
   
-    it "is invalid without a name" do
-      bad_user = User.new(name: nil, email: "noname@mail.com")
-      expect(bad_user).to_not be_valid
+    it "is invalid without a date" do
+      bad_entry = Entry.new(date: nil, body: "I have no date, but I have a user", user: subject)
+      expect(bad_entry).to_not be_valid
     end
   
-    it "is invalid without an email" do
-      bad_user = User.new(name: "Nomail", email: nil)
-      expect(bad_user).to_not be_valid
+    it "is invalid without a body" do
+      bad_entry = Entry.new(date: DateTime.now.utc, body: nil, user: subject)
+      expect(bad_entry).to_not be_valid
+    end
+
+    it "is invalid without a user" do
+      bad_entry = Entry.new(date: DateTime.now.utc, body: "Today there's no user :(", user: nil)
+      expect(bad_entry).to_not be_valid
     end
   end
 
   describe "Associations" do
-    it "should have many entries" do
-      assoc = User.reflect_on_association(:entries)
-      expect(assoc.macro).to eq :has_many
+    it "should belong to a use" do
+      assoc = Entry.reflect_on_association(:user)
+      expect(assoc.macro).to eq :belongs_to
     end
   end
 end

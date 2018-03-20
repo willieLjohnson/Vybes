6b125a3b9451d4416112afd71f2b3330a61c6673
SlickJohnson <liwa.johnson@gmail.com>
2018-03-20 10:54:16 -0700

    Update App controller to require auth

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  before_action :require_login
+
+  def require_login
+    authorize_request || render_unauthorized("Access denied")
+  end
+
+  def current_user
+    @current_user ||= authorize_request
+  end
+
+  def render_unauthorized(message)
+    errors = { errors: [ { detail: message } ] }
+    render json: errors, status: :unauthorized
+  end
+
+  private
+  def authorize_request
+    authenticate_with_http_token do |token, options|
+      User.find_by(token: token)
+    end
+  end
 end

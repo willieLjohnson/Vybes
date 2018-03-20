2c38368bd1a08f201eaa98d20a5dcc9a849accc6
SlickJohnson <liwa.johnson@gmail.com>
2018-03-20 11:50:55 -0700

    Write tests for User password

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -3,17 +3,22 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "Validations" do
     it "is valid with valid attributes" do
-      user = User.new(name: "Willie", email: "willie@test.com")
+      user = User.new(name: "Willie", email: "willie@test.com", password: "1234")
       expect(user).to be_valid
     end
   
     it "is invalid without a name" do
-      bad_user = User.new(name: nil, email: "noname@mail.com")
+      bad_user = User.new(name: nil, email: "noname@mail.com", password: "1234")
       expect(bad_user).to_not be_valid
     end
   
     it "is invalid without an email" do
-      bad_user = User.new(name: "Nomail", email: nil)
+      bad_user = User.new(name: "Nomail", email: nil, password: "1234")
+      expect(bad_user).to_not be_valid
+    end
+
+    it "is invalid without a password" do
+      bad_user = User.new(name: "Nomail", email: nil, password: nil)
       expect(bad_user).to_not be_valid
     end
   end

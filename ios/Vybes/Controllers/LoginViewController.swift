3c2680bb8d9cfc42463128c1f1284e7c9e51af89
SlickJohnson <liwa.johnson@gmail.com>
2018-03-25 01:13:13 -0700

    Add done buttons to login screen

--- ios/Vybes/Controllers/LoginViewController.swift
+++ ios/Vybes/Controllers/LoginViewController.swift
@@ -10,12 +10,23 @@ import UIKit
 
 /// Handles user signup and login
 class LoginViewController: UIViewController {
+  @IBOutlet weak var emailTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    view.addTapToDismissKeyboardGesture()
+    emailTextField.addDoneButtonOnKeyboard()
+    passwordTextField.addDoneButtonOnKeyboard()
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
+  }
+
+  @IBAction func loginButtonPressed(_ sender: UIButton) {
+    let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+    let viewController = storyBoard.instantiateViewController(withIdentifier: "ViewController")
+    present(viewController, animated: true, completion: nil)
   }
 }
